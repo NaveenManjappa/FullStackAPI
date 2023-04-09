55843dce4923937d12b58989f1f20ecd14dfdfa5
naveenkengannar <naveenkmforums@gmail.com>
2023-04-09 08:05:11 +0100

    Created apis for reading and creating employees with EF

---
+++ FullStackAPI/Controllers/EmployeesController.cs
@@ -0,0 +1,36 @@
+﻿using FullStackAPI.Data;
+using FullStackAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStackAPI.Controllers
+{
+    [ApiController]
+    [Route("/api/employees")]
+    public class EmployeesController : Controller
+    {
+        private readonly FullStackDbContext fullStackDbContext;
+
+        public EmployeesController(FullStackDbContext fullStackDbContext)
+        {
+            this.fullStackDbContext = fullStackDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllEmployees()
+        {
+           var employees=await fullStackDbContext.Employees.ToListAsync();
+            return Ok(employees);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee([FromBody]Employee employee)
+        {
+            //Random rand = new Random();
+            //employee.Id = rand.Next(4, 100);
+           await fullStackDbContext.Employees.AddAsync(employee);
+            await fullStackDbContext.SaveChangesAsync();
+            return Ok(employee);
+        }
+    }
+}
